2972c97f753a861725487909621b65bb40d8bea0
Introverting Turtle <levon.kc1@gmail.com>
2023-06-09 08:52:28 +0800

    solved 1 leetcode problem
    solved problem #744. Find Smallest Letter Greater Than Target

---
+++ src/Easy/find_smallest_letter_greater_than_target.js
@@ -0,0 +1,50 @@
+// You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+// Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+
+// Example 1:
+
+// Input: letters = ["c","f","j"], target = "a"
+// Output: "c"
+// Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+
+// Example 2:
+
+// Input: letters = ["c","f","j"], target = "c"
+// Output: "f"
+// Explanation: The smallest character that is lexicographically greater than 'c' in letters is 'f'.
+
+// Example 3:
+
+// Input: letters = ["x","x","y","y"], target = "z"
+// Output: "x"
+// Explanation: There are no characters in letters that is lexicographically greater than 'z' so we return letters[0]. 
+
+// Constraints:
+
+// 2 <= letters.length <= 104
+// letters[i] is a lowercase English letter.
+// letters is sorted in non-decreasing order.
+// letters contains at least two different characters.
+// target is a lowercase English letter.
+
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(letters, target) {
+    //Initialize result with the first element of letters (in case we dont find a match)
+    let result = letters[0]
+    let hasFound = false
+    //Iterate trough our lettes and find the letter that has a greater char code than our target
+    for(let i = 0; i < letters.length; i++) {
+        const current = letters[i]
+        if(current.charCodeAt(0) > target.charCodeAt(0) && !hasFound) {
+            //once a match is found, we set the boolean has found to true to stop our search
+            result = current
+            hasFound = true
+        }
+    }
+    //return the result
+    return result    
+};
